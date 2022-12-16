48726d8741b1796903180fb20f93c54fa9ec667a
Socrates Nguyen <socrates@weareenvoy.com>
2022-12-16 14:15:26 -0800

    add aoc2022-12.py

--- aoc2022-12.py
+++ aoc2022-12.py
@@ -1,3 +1,61 @@
+"""
+--- Day 12: Hill Climbing Algorithm ---
+You try contacting the Elves using your handheld device, but the river you're following must be too low to get a decent signal.
+
+You ask the device for a heightmap of the surrounding area (your puzzle input). The heightmap shows the local area from above broken into a grid; the elevation of each square of the grid is given by a single lowercase letter, where a is the lowest elevation, b is the next-lowest, and so on up to the highest elevation, z.
+
+Also included on the heightmap are marks for your current position (S) and the location that should get the best signal (E). Your current position (S) has elevation a, and the location that should get the best signal (E) has elevation z.
+
+You'd like to reach E, but to save energy, you should do it in as few steps as possible. During each step, you can move exactly one square up, down, left, or right. To avoid needing to get out your climbing gear, the elevation of the destination square can be at most one higher than the elevation of your current square; that is, if your current elevation is m, you could step to elevation n, but not to elevation o. (This also means that the elevation of the destination square can be much lower than the elevation of your current square.)
+
+For example:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Here, you start in the top-left corner; your goal is near the middle. You could start by moving down or right, but eventually you'll need to head toward the e at the bottom. From there, you can spiral around to the goal:
+
+v..v<<<<
+>v.vv<<^
+.>vv>E^^
+..v>>>^^
+..>>>>>^
+In the above diagram, the symbols indicate whether the path exits each square moving up (^), down (v), left (<), or right (>). The location that should get the best signal is still E, and . marks unvisited squares.
+
+This path reaches the goal in 31 steps, the fewest possible.
+
+What is the fewest steps required to move from your current position to the location that should get the best signal?
+
+Your puzzle answer was 472.
+
+--- Part Two ---
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+
+Again consider the example from above:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+This path reaches the goal in only 29 steps, the fewest possible.
+
+What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
+
+Your puzzle answer was 465.
+"""
+
 example_input = """Sabqponm
 abcryxxl
 accszExk
@@ -46,6 +104,8 @@ abaacccccccccccccccccccccccccccaaaaaacccaaaaaaaaaaaccaaacaaccaaaaaaaaaaaaaccccca
 abcccccccccccccccccccccccccccccaaaaaccccaacaacaaaaacccaaccccccaaaaaaaaaaaacccccaaaaacccccccccaaaaaaaccccccccccccccccccccccccccccccaaacccccccccccccccaaaaaa
 abcccccccccccccccccccccccccccaaaaaaaccccccccaaaaaaaaccccccccccaaaaaaaaaaccccccaaaaaaccccccccaaaaaaaaccccccccccccccccccccccccccccccccccccccccccccccccaaaaaa"""
 
+import heapq
+
 class Solution:
     @staticmethod
     def parse_input(puzzle_input):
@@ -55,27 +115,22 @@ class Solution:
     def __init__(self, puzzle_input):
         self.puzzle_input = puzzle_input
         self.grid = Solution.parse_input(puzzle_input)
-        self.min_distance = 0
-        self.set_end_position()
-        self.set_start_positions()
+        self.min_distances = []
+        self.set_positions()
         self.run()
 
-    def set_end_position(self):
-        grid = [list(s) for s in self.puzzle_input.splitlines()]
+    def set_positions(self):
+        start_positions = []
+        grid = self.puzzle_input.splitlines()
         for r, row in enumerate(grid):
             for c, item in enumerate(row):
                 if item == 'E':
                     self.end_position = (r, c)
-
-    def set_start_positions(self):
-        start_positions = []
-        for r, row in enumerate(self.puzzle_input):
-            for c, item in enumerate(row):
-                if item == 'a':
+                if item == 'a' or item == 'S':
                     start_positions.append((r, c))
+        self.start_positions = start_positions
 
-    def run(self):
-        sr, sc = self.start_position
+    def find_shortest_path(self, sr, sc):
         er, ec = self.end_position
         visited = {(sr, sc)}
         queue = [(sr, sc, 0)]
@@ -85,7 +140,7 @@ class Solution:
             current_value = self.grid[r][c]
 
             if r == er and c == ec:
-                self.min_distance = distance
+                heapq.heappush(self.min_distances, distance)
                 break
 
             directions = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
@@ -108,9 +163,13 @@ class Solution:
                 queue.append((nr, nc, distance + 1))
                 visited.add((nr, nc))
 
-    def get_answer(self):
-        return self.min_distance
+    def run(self):
+        for sr, sc in self.start_positions:
+            self.find_shortest_path(sr, sc)
 
-solution = Solution(example_input)
+    def get_answer(self):
+        return heapq.nsmallest(1, self.min_distances)[0]
+
+solution = Solution(puzzle_input)
 answer = solution.get_answer()
 print(answer)
